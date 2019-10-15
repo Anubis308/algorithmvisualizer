db6397225ea182e650d2c85ee52c088305f0c591
rifath <rifath.sahriar@selise.ch>
2019-10-15 15:31:42 +0600

    Chart added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SortGraphComponent } from './sort-graph/sort-graph.component';
 import {MatButtonModule} from '@angular/material/button';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sort-graph/sort-graph.component.ts
+++ src/app/sort-graph/sort-graph.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets, ChartAnimationOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'sort-graph',
@@ -8,28 +10,93 @@ import { Component, OnInit } from '@angular/core';
 export class SortGraphComponent implements OnInit {
   numberOfData:number;
   datas=[];
+  tempArray=[];
+  timeoutMilisecond:number;
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    responsiveAnimationDuration: 0,
+    scales: { xAxes: [{}], yAxes: [{}] },
+    plugins: {
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      }
+    },
+    animation:{
+      duration:0,
+    }
+    
+  }
+  
+  public barChartLabels: Label[] ;
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  
+
+  public barChartData: ChartDataSets[];
+
   constructor() { }
 
   ngOnInit() {
-    this.numberOfData=12;
-    this.datas=Array(this.numberOfData).fill(0).map(() => Math.round(Math.random() * 100))
+    this.numberOfData=120;
+    this.timeoutMilisecond=0;
+    this.datas=Array(this.numberOfData).fill(0).map(() => Math.round(Math.random() * 100));
+    this.barChartLabels=this.tempArray;
+    this.tempArray=this.datas;
+    this.barChartData = [
+      { data: this.tempArray}
+    ];
+    this.barChartLabels=this.tempArray;
     console.log(this.datas);
   }
 
   sort(){
     for(let i=0; i<this.numberOfData-1; i++){
-      for(let j=i+1; j<this.numberOfData; j++){
-        if(this.datas[i]>this.datas[j]){
-          let now = new Date().getTime();
-          while ( new Date().getTime() < now + 300){}
-
-          let temp=this.datas[i];
-          this.datas[i]=this.datas[j];
-          this.datas[j]=temp;
-          console.log(this.datas);
-        }
-      }
+      setTimeout(()=>{
+        this.innerSort(i);
+      },i *this.numberOfData* this.timeoutMilisecond);
+      
     }
   }
 
+  innerSort (i:number) {
+    for(let j=i+1; j<this.numberOfData; j++){
+      setTimeout(()=>{
+        this.innerSwap(i,j);
+      },j * this.timeoutMilisecond);
+      
+    }
+  }
+  innerSwap(i:number,j:number){
+    if(this.tempArray[i]>this.tempArray[j]){
+      /*let now = new Date().getTime();
+      while ( new Date().getTime() < now + 300){}*/
+
+      let temp=this.tempArray[i];
+      this.tempArray[i]=this.tempArray[j];
+      this.tempArray[j]=temp;
+      this.barChartData = [
+        { data: this.tempArray}
+      ];
+      //console.log(i,j);
+    }
+  }
+
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    this.datas=Array(this.numberOfData).fill(0).map(() => Math.round(Math.random() * 100));
+    this.tempArray=this.datas;
+    this.barChartData = [
+      { data: this.tempArray}
+    ];
+    this.barChartLabels=this.tempArray;
+  }
 }
